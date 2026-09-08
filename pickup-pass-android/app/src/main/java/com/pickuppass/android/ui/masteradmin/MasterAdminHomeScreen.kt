93c0bcb588029d8fab7d569394de36470df16fcd
mamhe25 <sheilamae.makinano625@gmail.com>
2026-09-08 13:15:10 +0800

    fix(android): import platform tools card border

--- pickup-pass-android/app/src/main/java/com/pickuppass/android/ui/masteradmin/MasterAdminHomeScreen.kt
+++ pickup-pass-android/app/src/main/java/com/pickuppass/android/ui/masteradmin/MasterAdminHomeScreen.kt
@@ -1,5 +1,6 @@
 package com.pickuppass.android.ui.masteradmin
 
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
